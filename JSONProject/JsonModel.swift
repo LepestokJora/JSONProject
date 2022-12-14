0bb3fa1a8043eac35f3fbb633555d75e4a98e467
Artem Peshkov <pewkoff@gmail.com>
2022-12-15 00:37:30 +0300

    totalJson

--- JSONProject/JsonModel.swift
+++ JSONProject/JsonModel.swift
@@ -6,5 +6,14 @@
 //
 
 import Foundation
-
-
+//4. В текущем ДЗ необходимо создать модель для парсинга джейсона
+struct ResponseData: Codable {
+    let url: String?
+    let title: String?
+    let service_version: String?
+    let media_type: String?
+    let hdurl: String?
+    let explanation: String?
+    let date: String?
+    let copyright: String?
+}
--- JSONProject/ViewController.swift
+++ JSONProject/ViewController.swift
@@ -7,50 +7,108 @@
 
 import UIKit
 
-//1. Выберите любой публичный API на свой вкус
-//2. данные которого вы должны будете в последующем отобразить в своем приложении.
-//3. Написать функцию запроса данных с сервера
-//4. В текущем ДЗ необходимо создать модель для парсинга джейсона
-//5. Распарсить JSON,
-//6. Отобразить данные на консоли.(вызвать фукцию принт из своей модели)
-
-
-
 class ViewController: UIViewController {
-
-    @IBOutlet weak var jsonLabel: UILabel!
     
+    @IBOutlet weak var jsonLabel: UILabel!
+    @IBOutlet weak var weitActIndView: UIActivityIndicatorView!
     @IBOutlet weak var startButton: UIButton!
     
+    enum StatusBrn {
+        case start, clean
+    }
+    
+    //1. Выберите любой публичный API на свой вкус данные которого вы должны будете в последующем отобразить в своем приложении.
     let urlJson: String = "https://go-apod.herokuapp.com/apod"
-    
-   
-    
+    private var statusBtn:StatusBrn = .start
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        weitActIndView.isHidden = true
+        weitActIndView.startAnimating()
     }
+    @IBAction func startAction() {
+        switch statusBtn {
+        case .start:
+            startJson()
+        case .clean:
+            self.jsonLabel.text = "Press start button"
+            statusBtn = .start
+            startButton.setTitle("Start", for: .normal)
+        }
 
-
+    }
+    
+    
+    
+    
+    //3. Написать функцию запроса данных с сервера
     private func startJson() {
-        
         guard let url = URL(string: urlJson) else { return }
-      
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        weitActIndView.isHidden = false
+        startButton.isUserInteractionEnabled = false
+        URLSession.shared.dataTask(with: url) { data, _, error in
             //если есть у меня данные и ответ а иначе вывожу ошибку и выхлжу
-            guard let data, let response else {
-                print(error?.localizedDescription ?? "No error discription")
+            guard error == nil, let data = data else {
+                print("No data received:", error ?? URLError(.badServerResponse))
+                self.startButton.isUserInteractionEnabled = true
                 return
             }
             // есть ответ в дате значить парссим
-            let jsonData = try JSONDecoder().decode(ResponseData.self, from: data)
-            print(response)
-        }
+            let decoder = JSONDecoder()
+            //5. Распарсить JSON,
+            do {
+                let jsonData = try decoder.decode(ResponseData.self, from: data)
+                //6. Отобразить данные на консоли.(вызвать фукцию принт из своей модели)
+                DispatchQueue.main.async {
+                    self.showResult(jsonData)
+                }
+            }
+            catch let parseError {
+                self.startButton.isUserInteractionEnabled = true
+                print("Parsing error:", parseError, String(describing: String(data: data, encoding: .utf8)))
+            }
+            
+        }.resume()
+        
         
     }
     
     
+    private func showResult(_ responseData:ResponseData?) {
+        var resultStr = ""
+        if let url = responseData?.url {
+            resultStr.append("url: \(url)\n\n")
+        }
+        if let title = responseData?.title {
+            resultStr.append("title: \(title)\n\n")
+        }
+        if let service_version = responseData?.service_version {
+            resultStr.append("service_version: \(service_version)\n\n")
+        }
+        if let media_type = responseData?.media_type {
+            resultStr.append("media_type: \(media_type)\n\n")
+        }
+        if let hdurl = responseData?.hdurl {
+            resultStr.append("hdurl: \(hdurl)\n\n")
+        }
+        if let explanation = responseData?.explanation {
+            resultStr.append("explanation: \(explanation)\n\n")
+        }
+        if let date = responseData?.date {
+            resultStr.append("date: \(date)\n\n")
+        }
+        if let copyright = responseData?.copyright {
+            resultStr.append("copyright: \(copyright)\n\n")
+        }
+        
+        self.jsonLabel.text = resultStr
+        weitActIndView.isHidden = true
+        startButton.setTitle("Clean", for: .normal)
+        statusBtn = .clean
+        DispatchQueue.main.asyncAfter(wallDeadline: .now() + 1, execute: {
+            self.startButton.isUserInteractionEnabled = true
+        })
+    }
     
 }
 
